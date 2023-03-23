292330f1a63f61bdac86f3820bb59699576a2c8d
Anton Charny <anton.charny@perimeter81.com>
2023-03-23 18:42:50 +0200

    Done

--- SwiftMarathon8/ViewController.swift
+++ SwiftMarathon8/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var imageView = UIImageView(image: UIImage(systemName: "person.crop.circle.fill")?
+        .withTintColor(.systemGray, renderingMode: .alwaysOriginal))
 
+    private lazy var scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.imageView.contentMode = .scaleAspectFit
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.title = "Avatar"
+        
+        self.scrollView.backgroundColor = .systemBackground
+        self.scrollView.contentSize = .init(width: self.view.bounds.width, height: self.view.bounds.height * 2)
+        self.view = self.scrollView
+        
+        self.addImage()
     }
-
-
+    
+    private func addImage() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        
+        for subview in navigationBar.subviews {
+            if let labelView = subview.subviews.first(where: { $0 is UILabel }) {
+                subview.addSubview(self.imageView)
+                
+                NSLayoutConstraint.activate([
+                    self.imageView.heightAnchor.constraint(equalToConstant: 36),
+                    self.imageView.widthAnchor.constraint(equalToConstant: 36),
+                    self.imageView.trailingAnchor.constraint(equalTo: subview.trailingAnchor, constant: -16),
+                    self.imageView.centerYAnchor.constraint(equalTo: labelView.centerYAnchor)
+                ])
+                return
+            }
+        }
+    }
 }
 
