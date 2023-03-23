bb9e2ae9171b1f1620f0e921112627a6db6863ce
Anton Charny <anton.charny@perimeter81.com>
2023-03-23 17:52:09 +0200

    Initial Commit

---
+++ SwiftMarathon8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMarathon8
+//
+//  Created by Anton Charny on 23/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
